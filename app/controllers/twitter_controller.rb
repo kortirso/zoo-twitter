345a8c52e09a762c4a5b76036ea4b83c670e5cde
Bogdanov Anton <kortirso@gmail.com>
2015-07-07 13:53:31 +0300

    add devise and simple_form support

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,4 +1,5 @@
 class TwitterController < ApplicationController
+	before_action :authenticate_user!
 
 	def index
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+	devise_for :users
 	get ':account' => 'twitter#account', as: 'account'
 	root to: 'twitter#index'
 	match "*path", to: "application#catch_404", via: :all
